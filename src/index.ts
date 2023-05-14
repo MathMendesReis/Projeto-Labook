6de4d61d0628ac6d6cd1e2ab7df55c5fcdeba20a
Matheus Mendes <matheusmendesreis@outlook.com>
2023-05-14 18:00:46 -0300

    create conexion knex

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express";
+
+//import do CORS 👇🏽
+import cors from "cors";
+
+//criação do servidor express 👇🏽
+const app = express();
+
+//configuração do middleware que garante que nossas respostas estejam sempre
+//no formato json 👇🏽
+app.use(express.json());
+
+//configuração do middleware que habilita o CORS 👇🏽
+app.use(cors());
+
+app.listen(3003, () => {
+  console.log(`Servidor rodando na porta ${3003}`);
+});
+
+app.get("/ping", async (req: Request, res: Response) => {
+  try {
+    res.status(200).send({ message: "Pong!" });
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
+    }
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
